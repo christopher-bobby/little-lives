5aa9031fd6323c2ebddc2037d61b3be1e7613642
Christopher <christopher@Christophers-MacBook-Pro.local>
2024-08-25 22:53:22 +0700

    remove unused

--- src/main.js
+++ src/main.js
@@ -5,10 +5,7 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 
-/* add icons to the library */
-library.add(fasHeart, farHeart)
-
-const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon)
+const app = createApp(App)
 
 app.use(router)
 
